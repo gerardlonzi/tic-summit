2d694582617b1a17cd13c1ad70f29dccac333340
DoretteButame <butamedorette22@gmail.com>
2023-08-11 15:38:08 +0200

    summit website final

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css'
-import Barner from './conponents/blocks-pages/barner'
-import About from './conponents/blocks-pages/about'
-import Get from './conponents/blocks-pages/get'
-import Sponsor from './conponents/blocks-pages/Sponsor'
-import Stats from './conponents/blocks-pages/Stats'
-import Slide from './conponents/blocks-pages/Slide'
-import Insights from './conponents/blocks-pages/Insgshits'
-import Testimo from './conponents/blocks-pages/Testimo'
-import Startup from './conponents/blocks-pages/Startup'
-import Answer from './conponents/blocks-pages/answer'
-import Contact from './conponents/blocks-pages/Contact'
-import Footer from './conponents/blocks-pages/footer'
+import "./App.css";
+import Barner from "./conponents/blocks-pages/barner";
+import About from "./conponents/blocks-pages/about";
+import Get from "./conponents/blocks-pages/get";
+import Sponsor from "./conponents/blocks-pages/Sponsor";
+import Stats from "./conponents/blocks-pages/Stats";
+import Slide from "./conponents/blocks-pages/Slide";
+import Insights from "./conponents/blocks-pages/Insgshits";
+import Testimo from "./conponents/blocks-pages/Testimo";
+import Startup from "./conponents/blocks-pages/Startup";
+import Answer from "./conponents/blocks-pages/answer";
+import Contact from "./conponents/blocks-pages/Contact";
+import Footer from "./conponents/blocks-pages/footer";
 
 function App() {
 
@@ -29,7 +29,7 @@ function App() {
       <Contact />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/conponents/blocks-pages/Slide.jsx
+++ src/conponents/blocks-pages/Slide.jsx
@@ -10,19 +10,21 @@ import v4 from './V1 (4).png'
 export default function Slide() {
   return (
     <>
-    <div className="event">
+      <div className="event">
         <p>Past Event</p>
-    </div>
-    <div className="slides">
+      </div>
+      <div className="slides">
         <div className="slide">
-            <img src={v1} alt="" />
-            <img src={v2} alt="" />
-            <img src={v3} alt="" />
-            <img src={v4} alt="" />
+          <img src={v1} alt="" />
+          <img src={v2} alt="" />
+          <img src={v3} alt="" />
+          <img src={v4} alt="" />
+          <img src={v1} alt="" />
+          <img src={v2} alt="" />
+          <img src={v3} alt="" />
+          <img src={v4} alt="" />
         </div>
-    </div>
-    
-    
+      </div>
     </>
-  )
+  );
 }
